496c8a5ced09fb6435c0859e03b74dd159cdc92a
denispham1107 <denispham1107@users.noreply.github.com>
2026-09-04 17:20:18 +0700

    Update Unity WebGL build phuthuy-mobile11 (2026-09-04)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const GAME_BUILD_VERSION = "20260903-2d744b0dde70";
+const GAME_BUILD_VERSION = "20260904-1e15528b72a1";
 const CACHE_PREFIX = "phuthuy3d-shell-";
 const LEGACY_CACHE_PREFIXES = ["diablo25d-shell-"];
 const SHELL_CACHE = `${CACHE_PREFIX}${GAME_BUILD_VERSION}`;
